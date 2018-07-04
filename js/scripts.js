b45cdbb6bd4d21ccce71ba5e3b9a021ed7eda6c7
MakerJoseph <1joelmaker@gmail.com>
2018-07-04 10:36:23 +0300

    jquery intro to create pop ups on the browser when you click on different elements

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("h1").click(function(){
+      alert("This is a heading");
+      alert("I told you this a HEADING!");
+    })
+
+    $("p").click(function(){
+      alert("this is a paragraph");
+  });
+
+    $("img").click(function(){
+      alert("This is an image");
+    });
+});
